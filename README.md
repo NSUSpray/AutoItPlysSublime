bd90ddc1d5b53291fa9f31ece011622885a84e22
NSUSpray <nsu.spray@gmail.com>
2021-10-31 19:41:36 +0800

    fix compile command name; autosetup autoit paths; readable syntax files; compact tools menu

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
-# SublimeAutoItScript - Package for Sublime Text 2/3
-AutoItScript Au3 language package for SublimeText including syntax highlighting, comments toggling, auto-completions, build systems for run and compile, Tidy command, IncludeHelper command.
+# AutoItPlysSublime - Package for Sublime Text 2–4
+AutoIt language and Plys dialect package for Sublime Text including syntax highlighting, comments toggling, auto-completions, build systems for run and compile, Tidy command, Include Helper command.
 
 ## Package Installation
-* Manual method: Download ZIP from github. Extract the files to [Sublime_Data_Dir](http://docs.sublimetext.info/en/latest/basic_concepts.html#the-data-directory)\Packages\AutoItScript
-* Automatic method: Use [wbond package control](https://sublime.wbond.net/).
+* Manual method:
+	1. download ZIP from this repository
+	2. extract the files to Sublime packages folder (Menu: `Preferences > Browse Packages…`)
+* Package control method:
+	1. open Sublime Text
+	2. go to Command Palette (<kbd>Ctrl+Shift+P</kbd>) and enter “Add Repository”
+	3. enter this repository address `https://github.com/NSUSpray/AutoItPlysSublime`
+	4. in Command Palette again enter “Install Package”
+	5. type “AutoItPlysSublime”, select appropriate item and press Enter
 
 ## Key Bindings
 If you have the default Sublime keybindings intact, then:
 * <kbd>Ctrl+B</kbd> will run the current file (with AutoIt3.exe)
 * <kbd>Ctrl+Shift+B</kbd> will compile the current file (with Aut2Exe.exe)
 * <kbd>Alt+T</kbd><kbd>T</kbd> will invoke Tidy on the current file (with Tidy.exe).
-* <kbd>Alt+T</kbd><kbd>I</kbd> will invoke IncludeHelper on the current file.
+* <kbd>Alt+T</kbd><kbd>I</kbd> will invoke Include Helper on the current file.
 * <kbd>Alt+T</kbd><kbd>F</kbd> will invoke AutoIt Window Info.
 * <kbd>Alt+T</kbd><kbd>H</kbd> will invoke AutoIt Help.
 
@@ -24,4 +31,4 @@ Instructions on how to configure goto-documentation plugin for AutoIt (F1 Hotkey
 ## Credits
 * Syntax rules: http://sublime-text-community-packages.googlecode.com/svn/pages/AutoIt.html
 * Snippets: http://www.autoitscript.com/forum/topic/148016-sublimetext/page-3#entry1080276
-* IncludeHelper AZJIO: http://www.autoitscript.com/forum/topic/130468-constants-helper/#entry908064
+* Include Helper AZJIO: http://www.autoitscript.com/forum/topic/130468-constants-helper/#entry908064
--- autoitbuild.py
+++ autoitbuild.py
@@ -7,31 +7,37 @@ import os
 class autoitbuild(sublime_plugin.WindowCommand):
 	def run(self):
 		filepath = self.window.active_view().file_name()
-		AutoItExePath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItExePath")
-		cmd = [AutoItExePath, "/ErrorStdOut", filepath]
+		autoit_exe_path = \
+			sublime.load_settings("AutoIt.sublime-settings").get("AutoItExePath")
+		cmd = [autoit_exe_path, "/ErrorStdOut", filepath]
 		self.window.run_command("exec", {"cmd": cmd})
 
 class autoitcompile(sublime_plugin.WindowCommand):
 	def run(self):
 		filepath = self.window.active_view().file_name()
-		AutoItCompilerPath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItCompilerPath")
-		cmd = [AutoItCompilerPath, "/in", filepath]
+		autoit_compiler_path = \
+			sublime.load_settings("AutoIt.sublime-settings").get("AutoItCompilerPath")
+		cmd = [autoit_compiler_path, "/in", filepath]
 		self.window.run_command("exec", {"cmd": cmd})
 
 class autoittidy(sublime_plugin.WindowCommand):
 	def run(self):
 		self.window.run_command("save")
 		filepath = self.window.active_view().file_name()
-		TidyExePath = sublime.load_settings("AutoIt.sublime-settings").get("TidyExePath")
-		tidycmd = [TidyExePath, filepath]
+		tidy_exe_path = \
+			sublime.load_settings("AutoIt.sublime-settings").get("TidyExePath")
+		tidycmd = [tidy_exe_path, filepath]
 		try:
-			tidyprocess = subprocess.Popen(tidycmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+			tidyprocess = subprocess.Popen(
+				tidycmd, shell=True, stdout=subprocess.PIPE,
+				stderr=subprocess.STDOUT, universal_newlines=True
+			)
 			tidyoutput = tidyprocess.communicate()[0].rstrip()
 			tidyoutputskipfirstline = "".join(tidyoutput.splitlines(True)[1:])
 			self.window.run_command("revert")
 			print("------------ Beginning AutoIt Tidy ------------")
 			print(tidyoutput)
-			if("Tidy Error" in tidyoutput):
+			if "Tidy Error" in tidyoutput:
 				sublime.active_window().run_command("show_panel", {"panel": "console"})
 				sublime.status_message("### Tidy Errors : Please See Console")
 			else:
@@ -40,7 +46,7 @@ class autoittidy(sublime_plugin.WindowCommand):
 			sublime.active_window().run_command("show_panel", {"panel": "console"})
 			print("------------ ERROR: Python exception trying to run Tidy ------------")
 			print("TidyCmd was: " + " ".join(tidycmd))
-			print("Error {0}".format(str(e)))
+			print("Error " + str(e))
 			sublime.status_message("### EXCEPTION: " + str(e))
 
 class autoitincludehelper(sublime_plugin.WindowCommand):
@@ -48,44 +54,101 @@ class autoitincludehelper(sublime_plugin.WindowCommand):
 		self.window.run_command("save")
 
 		filepath = self.window.active_view().file_name()
-		AutoItExePath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItExePath")
-		AutoItIncludeFolder = os.path.dirname(AutoItExePath) + "\\Include"
+		autoit_exe_path = \
+			sublime.load_settings("AutoIt.sublime-settings").get("AutoItExePath")
+		autoit_include_folder = os.path.dirname(autoit_exe_path) + "\\Include"
 
-		IncludeHelperAU3Path = sublime.load_settings("AutoIt.sublime-settings").get("IncludeHelperAU3Path")
-		if (IncludeHelperAU3Path is None):
-			IncludeHelperAU3Path = "{PACKAGE_PATH}\\AutoItScript\\Include_Helper.au3"
-		IncludeHelperAU3Path = IncludeHelperAU3Path.replace("{PACKAGE_PATH}", sublime.packages_path())
+		include_helper_path = \
+			sublime.load_settings("AutoIt.sublime-settings").get("IncludeHelperAU3Path")
+		if include_helper_path is None:
+			include_helper_path = \
+				"{PACKAGE_PATH}\\AutoItPlysSublime\\Include_Helper.au3"
+		include_helper_path = include_helper_path.replace(
+			"{PACKAGE_PATH}", sublime.packages_path()
+		)
 
-		AutoItIncludeCmd = [AutoItExePath, IncludeHelperAU3Path, filepath, AutoItIncludeFolder]
+		autoit_include_cmd = [autoit_exe_path, include_helper_path, filepath,
+			autoit_include_folder]
 
 		try:
-			subprocess.call(AutoItIncludeCmd)
+			subprocess.call(autoit_include_cmd)
 			self.window.run_command("revert")
 			sublime.status_message("AutoIt IncludeHelper Finished")
 		except Exception as e:
 			sublime.active_window().run_command("show_panel", {"panel": "console"})
 			print("------------ ERROR: Python exception trying to run following command ------------")
-			print(AutoItIncludeCmd)
-			print("Error {0}".format(str(e)))
+			print(autoit_include_cmd)
+			print("Error " + str(e))
 
-class autoitinfo(sublime_plugin.WindowCommand):
+class autoitwindowinfo(sublime_plugin.WindowCommand):
 	def run(self):
-		AutoItInfo = sublime.load_settings("AutoIt.sublime-settings").get("AutoItInfo")
+		autoit_info_path = \
+			sublime.load_settings("AutoIt.sublime-settings").get("AutoItInfoPath")
 
 		try:
-			os.startfile(AutoItInfo)
+			os.startfile(autoit_info_path)
 		except Exception as e:
 			sublime.active_window().run_command("show_panel", {"panel": "console"})
 			print("------------ ERROR: Python exception trying to run following command ------------")
-			print("Error {0}".format(str(e)))
+			print("Error " + str(e))
 
 class autoithelp(sublime_plugin.WindowCommand):
 	def run(self):
-		AutoItHelp = sublime.load_settings("AutoIt.sublime-settings").get("AutoItHelp")
+		autoit_help_path = \
+			sublime.load_settings("AutoIt.sublime-settings").get("AutoItHelpPath")
 
 		try:
-			os.startfile(AutoItHelp)
+			os.startfile(autoit_help_path)
 		except Exception as e:
 			sublime.active_window().run_command("show_panel", {"panel": "console"})
 			print("------------ ERROR: Python exception trying to run following command ------------")
-			print("Error {0}".format(str(e)))
+			print("Error " + str(e))
+
+class autoitgetpaths(sublime_plugin.WindowCommand):
+	def __init__(self, window):
+		super().__init__(window)
+
+		import winreg
+		def get_au3_exe_folder(bitness):
+			registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, \
+				r"SOFTWARE\AutoIt v3\AutoIt", 0, \
+				winreg.KEY_READ | winreg.KEY_WOW64_32KEY)
+			value, regtype = winreg.QueryValueEx(registry_key, "InstallDir")
+			winreg.CloseKey(registry_key)
+			return value
+		try:
+			au3_exe_folder = get_au3_exe_folder(winreg.KEY_WOW64_32KEY)
+		except WindowsError as e:
+			try:
+				au3_exe_folder = get_au3_exe_folder(winreg.KEY_WOW64_64KEY)
+			except WindowsError as e:
+				sublime.active_window().run_command("show_panel", {"panel": "console"})
+				print("------------ ERROR: Python exception trying to run following command ------------")
+				print("Error " + str(e))
+				return
+
+		import json
+		default_settings_path = sublime.packages_path() + \
+			"\\SublimeAutoItScript\\AutoIt.sublime-settings"
+		with open(default_settings_path) as f:
+			settings = json.load(f)
+		if not settings.get("AutoItExePath"):
+			settings["AutoItExePath"] = au3_exe_folder + "\\AutoIt3.exe"
+		if not settings.get("AutoItCompilerPath"):
+			settings["AutoItCompilerPath"] = \
+				au3_exe_folder + "\\Aut2Exe\\Aut2exe.exe"
+		if not settings.get("TidyExePath"):
+			settings["TidyExePath"] = au3_exe_folder + "\\SciTE\\Tidy\\Tidy.exe"
+		if not settings.get("AutoItInfoPath"):
+			import platform
+			is_os_64bit = platform.machine().endswith('64')
+			suffix = "_x64" if is_os_64bit else ""
+			settings["AutoItInfoPath"] = \
+				au3_exe_folder + "\\Au3Info" + suffix + ".exe"
+		if not settings.get("AutoItHelpPath"):
+			settings["AutoItHelpPath"] = au3_exe_folder + "\\AutoIt3Help.exe"
+		with open(default_settings_path, "w") as f:
+			json.dump(settings, f, indent="\t")
+
+	def run(self):
+		pass
